1beedb02977979cc0fa6940e1df08175aaced40a
Prashant Kumar <prashant63957174@gmail.com>
2025-03-02 18:51:17 +0530

    landing page completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
+import Hero from './components/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>
-        HELLO JEE
-      </h2>
-      <Button>cLICK ME</Button>
+         {/* Hero*/}
+         <Hero/>
     </>
   )
 }
